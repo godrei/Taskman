e64357c8bde972c2c69fc2c508f5851685858080
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-03-18 12:21:07 +0100

    Create the server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,4 +18,6 @@ dependencies {
     implementation(libs.ktor.server.netty)
     testImplementation(libs.ktor.server.tests)
     testImplementation(libs.kotlin.test.junit)
+    implementation(libs.ktor.serialization)
+    implementation(libs.ktor.server.content.negotiation)
 }
---
+++ server/src/main/kotlin/io/bitrise/taskman/model/TaskRepository.kt
@@ -0,0 +1,12 @@
+package io.bitrise.taskman.model
+
+import Priority
+import Task
+
+interface TaskRepository {
+    fun allTasks(): List<Task>
+    fun tasksByPriority(priority: Priority): List<Task>
+    fun taskByName(name: String): Task?
+    fun addOrUpdateTask(task: Task)
+    fun removeTask(name: String): Boolean
+}
